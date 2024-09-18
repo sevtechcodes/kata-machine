e14539e6e79e7c177ce7c252b3bd12d2a1619ec1
sevtechcodes <sevimtas0101@gmail.com>
2024-09-18 18:36:45 +0200

    Bubble Sort Algorithm

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,19 @@
-export default function bubble_sort(arr: number[]): void {
+// Sudo code:
+//[ 0, ...... , N] arr, we compare i with (i+1) to swap
+// for i ...n
+//	for j ... n-1-i 
+//		if (arr[i]> arr[j]){ swap(i,j)}
 
+
+export default function bubble_sort(arr: number[]): void {
+	for (let i = 0; i < arr.length; ++i) {
+		for (let j = 0; j < arr.length - 1 - i; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				const temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
 }
+
