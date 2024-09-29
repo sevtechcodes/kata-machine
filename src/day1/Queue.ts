a13446e1a5c2a010283baa53b3da3896123bd123
sevtechcodes <sevimtas0101@gmail.com>
2024-09-29 12:27:12 +0200

    Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,54 @@
+type Node<T> = {
+	value: T,
+	next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+	public length: number;
+	private head?: Node<T>;
+	private tail?: Node<T>;
 
-    
+	constructor() {
+		this.head = this.tail = undefined;
+		this.length = 0;
+	}
 
-    constructor() {
-    }
+	//adding a node to the end of the queue
+	enqueue(item: T): void {
+		const node = { value: item } as Node<T>;
+		this.length++;
+		if (!this.tail || !this.head) {
+			this.tail = this.head = node;
+			return;
+		}
+		this.tail.next = node;
+		this.tail = node;
 
-    enqueue(item: T): void {
+	}
 
-}
-    deque(): T | undefined {
+	//removing a node from the begining of the queue
+	deque(): T | undefined {
+		if (!this.head) {
+			return undefined;
+		}
 
-}
-    peek(): T | undefined {
+		this.length--;
 
-}
+		const head = this.head;
+		this.head = this.head.next;
+
+		//Optional: The JS garbage collector will take care of this part so actually we do not need to do this.
+		head.next = undefined;
+
+		if (this.length === 0) {
+			this.tail = undefined;
+		}
+
+		return head.value;
+	}
+
+	// Peek at the front value without removing it
+	peek(): T | undefined {
+		return this.head?.value;
+	}
 }
